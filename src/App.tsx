9467a54357c9342e502b74cdcd1b9b10539150ee
Deivid Almeida <mr.davialmeida@hotmail.com>
2022-08-24 14:08:13 -0300

    :recycle: code refatoration

--- src/App.tsx
+++ src/App.tsx
@@ -2,75 +2,96 @@ import React, { useRef } from 'react';
 import './App.css';
 import jsQR from "jsqr"; 
 
-
 function App() {
- 
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const canvas = canvasRef.current!.getContext("2d");
-  
   const videoRef = useRef<HTMLVideoElement>(null);
-  const chunks: any[] = [];
-
+  const chunks: Blob[] = [];
+  
   const storeFrames = async (frame: BlobEvent) => {
     chunks.push(frame.data)
-    await createUrl()
+    await gerateUrl()
   }
 
-  const createUrl = async () => {
-    let blob = new Blob(chunks, {
+  const gerateUrl = async () => {
+    const blob = new Blob(chunks, {
       type: chunks[0].type
     })
-
-    let url = URL.createObjectURL(blob)
+    const url = URL.createObjectURL(blob)
     videoRef.current!.src = await url
+  }
+  
+  const gerateImage = () => {
+    const canvas = canvasRef.current!.getContext("2d");    
+    canvas!.drawImage(videoRef.current  as unknown as HTMLCanvasElement , 0, 0, 300, 300)
+    const data =  canvas!.getImageData(
+      0,
+      0,
+      300,
+      300
+    );
+    return data;
+  }
+
+  const readQrCode = () => {  
+    const imageData = gerateImage();            
+    const code =  jsQR(imageData.data, imageData.width, imageData.height);
+      console.log(code);
+
+    return code;
+  };
+  const stopScanning = async () => {
+
   }
 
   const startScanning = async () => {
-    let stream = await navigator.mediaDevices.getDisplayMedia({
+    const stream = await navigator.mediaDevices.getDisplayMedia({
       video: true
-    })
-    
-    let mediaRecorder = new MediaRecorder(stream, {
+    });    
+    const mediaRecorder = new MediaRecorder(stream, {
       mimeType: "video/webm"
-    })
-    mediaRecorder.addEventListener('dataavailable',  async function(event: BlobEvent) {
-      
+    });
+    
+    mediaRecorder.addEventListener('dataavailable',  async function(event: BlobEvent) {      
       await  storeFrames(event)
 
       videoRef.current!.oncanplay = () => {
-        setTimeout( () => {
-          
-          canvas!.drawImage(videoRef.current  as unknown as HTMLCanvasElement , 0, 0, 300, 300)
-          var imageData =  canvas!.getImageData(
-            0,
-            0,
-            300,
-            300
-          );
-            
-            var code =  jsQR(imageData.data, imageData.width, imageData.height);
-            console.log(code);
-            if (code && mediaRecorder.state === "recording") {
+
+        setTimeout( () => {          
+          const code = readQrCode();
+
+          if (code && mediaRecorder.state === "recording") {
+            // TODO stopScanning()
             mediaRecorder.stop();
             stream.getTracks().forEach( (track:any) => track.stop() );
           }
-        }, 500);        
+        }, 500);
+
       }
+
     })
+
     mediaRecorder.start(1000) 
   }
   
   return (
     <div>
+
       <video 
         src=""
         ref={videoRef}
       />
-      <canvas ref={canvasRef} height={300} width={300} />
+
+      <canvas 
+        ref={canvasRef} 
+        height={300} 
+        width={300} 
+      />
+
       <button
         onClick={startScanning}
-      >start2
+      >start
       </button>
+
     </div>
 
   );
